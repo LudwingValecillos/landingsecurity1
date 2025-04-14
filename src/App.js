cb300a8a78eba5ecf46fb22cbe55c27f438399d0
ludwingval <ludwingval@gmail.com>
2025-04-14 18:28:06 +0000

    a

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./styles/App.css";
-import DentalLandingPage from "./components/SecurityLandinPage";
+import SecurityLandingPage from "./components/SecurityLandinPage";
 
 function App() {
-  return <DentalLandingPage />;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/security" element={<SecurityLandingPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h1>Welcome!</h1>
+      <Link to="/security"><button>Go to Security Page</button></Link>
+    </div>
+  );
 }
 
 export default App;
